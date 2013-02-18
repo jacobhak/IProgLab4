7b3bf74ff15a6c05f54fa17ae98e802a389b47dd
jacobhak <jacobhakansson@gmail.com>
2013-02-18 17:29:43 +0100

    Fixed popup bug

--- view/popupViewController.js
+++ view/popupViewController.js
@@ -1,6 +1,6 @@
 function PopupViewController(view,dishModel,dinnerModel) {
 	view.chooseButton.click(function(){
 		dinnerModel.addDishToMenu(view.dish.id);
-		$('#popup').modal('toggle');
+		$('#popup').modal('hide');
 	});
 }
